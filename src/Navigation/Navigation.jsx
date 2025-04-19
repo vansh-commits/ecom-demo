9ae8e0c474f9183d2af067326a1c94eaf1a2c279
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-19 15:07:42 +0530

    made a css reset in index.css file

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import "../index.css"
+import { FiHeart } from 'react-icons/fi'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { AiOutlineUserAdd } from 'react-icons/ai'
 
 const Navigation = () => {
   return (
     <nav>
-      <div className='nav-container'>
+      <div className="nav-container">
         <input type="text" placeholder='Enter your search Shoe' className='search-input' />
       </div>
       <div className='profile-container'>
         <a href='#'> 
-          
+          <FiHeart className='nav-icons' />
+        </a>
+        <a href='#'> 
+          <AiOutlineShoppingCart className='nav-icons' />
+        </a>
+        <a href='#' className=""> 
+          <AiOutlineUserAdd className='nav-icons' />
         </a>
       </div>
     </nav>

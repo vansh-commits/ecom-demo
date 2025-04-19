7b62a1d3fbd59ed32f31169dde607aa04bb97fcc
vansh-commits <vansh.malhotra439@hotmail.com>
2025-04-19 15:15:50 +0530

    altered styling

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -5,19 +5,19 @@ import { AiOutlineUserAdd } from 'react-icons/ai'
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className='flex justify-around items-center border-b-2 border-[#f3f3f3] p-5 z-[999] ml-8'>
       <div className="nav-container">
-        <input type="text" placeholder='Enter your search Shoe' className='search-input' />
+        <input type="text" placeholder='Enter your search Shoe' className='py-3 px-5 border-none bg-[#f7f6f6] outline-none mr-5 rounded relative w-56' />
       </div>
-      <div className='profile-container'>
+      <div className='flex flex-row items-center justify-center gap-4'>
         <a href='#'> 
-          <FiHeart className='nav-icons' />
+          <FiHeart className='w-6 h-6 ml-8' />
         </a>
         <a href='#'> 
-          <AiOutlineShoppingCart className='nav-icons' />
+          <AiOutlineShoppingCart className='w-6 h-6 ml-8' />
         </a>
         <a href='#' className=""> 
-          <AiOutlineUserAdd className='nav-icons' />
+          <AiOutlineUserAdd className='w-6 h-6 ml-8' />
         </a>
       </div>
     </nav>
